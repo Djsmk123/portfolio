668d4cfed9f043c1b137f3c11c13776b729702fc
MD Mobin <74850678+Djsmk123@users.noreply.github.com>
2023-06-10 13:52:59 +0530

    fix counter issue

--- src/views_count.js
+++ src/views_count.js
@@ -7,30 +7,22 @@ function ViewsCounter() {
       // check if the user is on localhost
       if (window.location.hostname === "localhost") {
         setCount(0);
-        return;
       }
+    
       else{ 
-
-        //check if count is already stored in local storage
-        if (localStorage.getItem("count") !== null && localStorage.getItem("count") !== undefined) {
-          setCount(localStorage.getItem("count"));
-          return;
-        } 
-        // if not, get the count from the api
-        else{
-          axios.get("https://api.countapi.xyz/hit/smkwinner.vercel.app/visits")
-          .then(res => {
-            setCount(res.data.value);
-            console.log("Views: " + res.data.value);
-            // store the count in local storage
-            localStorage.setItem("count", res.data.value);
-          })
-        
-          .catch(err => {
-            console.log(err);
-          }) 
+        console.log(localStorage.getItem("count")!== undefined);
+      
+        if(!localStorage.getItem("count")!== undefined){
+          axios.get("https://counterapi.com/api/smkwinner.vercel.app/view?readonly=true&startNumber=450").then((res)=>{
+          setCount(res.data.value);
+          localStorage.setItem("count", res.data.value);
+         });
+        }else{
+          axios.get("https://counterapi.com/api/smkwinner.vercel.app/view?startNumber=450").then((res)=>{
+          setCount(res.data.value);
+          localStorage.setItem("count", res.data.value);
+         });
         }
-       
       }
      
     }, []);
@@ -43,3 +35,33 @@ function ViewsCounter() {
   }
 
 export default ViewsCounter;
+
+/*if (localStorage.getItem("count") !== null && localStorage.getItem("count") !== undefined) {
+          axios.get("https://counterapi.com/api/mysite.com/view?readonly=true&startNumber=450")  // startNumber from last API (view were 450)
+          .then(res => {
+            setCount(res.value);
+            // store the count in local storage
+            localStorage.setItem("count", res.data.value);
+          })
+        
+          .catch(err => {
+            console.log(err);
+          }) 
+          setCount(localStorage.getItem("count"));
+        
+        } 
+        // if not, get the count from the api
+        else{
+          axios.get("https://counterapi.com/api/mysite.com/view?startNumber=450")  // startNumber from last API (view were 450)
+          .then(res => {
+            setCount(res.value);
+            console.log("Views: " + res.data.value);
+            // store the count in local storage
+            localStorage.setItem("count", res.data.value);
+          })
+        
+          .catch(err => {
+            console.log(err);
+          }) 
+        
+        }*/
